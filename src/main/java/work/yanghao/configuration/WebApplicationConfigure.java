2bbfc892f0f6f9acc941eb25c3e02c151d8e20be
yanghao <80003422@sfmail.sf-express.com>
2019-02-28 17:01:00 +0800

    更新部分说明

--- src/main/java/work/yanghao/configuration/WebApplicationConfigure.java
+++ src/main/java/work/yanghao/configuration/WebApplicationConfigure.java
@@ -2,11 +2,26 @@ package work.yanghao.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import work.yanghao.interceptor.MVCInterceptor;
 
+/**
+ * 配置springMVC的bean
+ *
+ */
 @Configuration
 public class WebApplicationConfigure implements WebMvcConfigurer {
+
+    /**
+     * 先前跳转页面需要在Controller中写一个方法，现在通过configureViewResolvers可以不用写控制器就能达到目的了
+     * @param registry
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
